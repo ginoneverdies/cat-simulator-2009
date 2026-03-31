b4d099d353af7d5d036985b34fe93acd941ed3a3
ginoneverdies <ginoneverdies@gmail.com>
2026-03-31 20:16:37 +0200

    Create GameMain.java for main game entry point

---
+++ GameMain.java
@@ -0,0 +1,6 @@
+public class GameMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cat Simulator 2009!");
+        // Initialize game components here
+    }
+}
